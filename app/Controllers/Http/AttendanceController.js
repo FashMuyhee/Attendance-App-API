19eb739e81cb999cd7aa872117127117cda61a59
fashmuyhee <fashmuyidavid@gmail.com>
2020-02-28 02:28:20 +0100

    created createSignout and signout funtion

--- app/Controllers/Http/AttendanceController.js
+++ app/Controllers/Http/AttendanceController.js
@@ -3,6 +3,7 @@ const Attendance = use("App/Models/Attendance");
 const Student = use("App/Models/Student");
 const User = use("App/Models/User");
 const { validate } = use("Validator");
+const randomstring = require("randomstring");
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
@@ -26,7 +27,11 @@ class AttendanceController {
         const user = await auth.getUser();
         const lecturer = await user.lecturer().fetch();
         const { course_id, location } = request.all();
-        const code = 1233
+        const code = randomstring.generate({
+          length: 7,
+          charset: "hex",
+          capitalization: "uppercase"
+        });
         const attendance = JSON.stringify(new Array());
         const data = {
           code: code,
@@ -50,8 +55,8 @@ class AttendanceController {
         await lecturer.myAttendances().create(data);
 
         return response.status(200).send({
-          // payload: { type: "success", message: "attendance created" }
-          payload: { type: "success", message: data}
+          payload: { type: "success", message: "attendance created" }
+          // payload: { type: "success", message: data }
         });
       } catch (error) {
         return response.status(error.status).send({
@@ -89,7 +94,8 @@ class AttendanceController {
         const element = data[index];
         if (
           element.hasOwnProperty("student_id") &&
-          student.id === element.student_id
+          student.id === element.student_id &&
+          element.signed_in
         ) {
           return response.status(200).send({
             payload: {
@@ -100,7 +106,12 @@ class AttendanceController {
         }
       }
       // else add student to the attendance list
-      data.push({ student_id: student.id, gps });
+      data.push({
+        student_id: student.id,
+        gps,
+        signed_in: true,
+        signed_out: false
+      });
       query.attendance = JSON.stringify(data);
       /* 
     const validation = await validate(data, rules);
@@ -115,6 +126,84 @@ class AttendanceController {
         payload: {
           type: "success",
           message: `attendance submitted`
+        }
+      });
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ payload: { type: "error", error } });
+    }
+  }
+
+  /**
+   * Update attendance details.
+   * PUT or PATCH attendances/signout
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async createSignout({ params: { code }, response, auth }) {
+    try {
+      await auth.check();
+
+      const user = await auth.getUser();
+      const lecturer = await user.lecturer().fetch();
+      const signout_code = randomstring.generate({
+        length: 7,
+        charset: "hex",
+        capitalization: "uppercase"
+      });
+      const query = await Attendance.findBy("code", code);
+      query.signout_code = signout_code;
+      /*
+    const validation = await validate(data, rules);
+    if (validation.fails()) {
+      return response
+        .status(400)
+        .send({ payload: { type: "error", error: validation.messages() } });
+    }
+ */
+      await query.save(query);
+
+      return response.status(200).send({
+        payload: {
+          type: "success",
+          message: `signout code created`
+        }
+      });
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ payload: { type: "error", error } });
+    }
+  }
+  /**
+   * Update attendance details.
+   * PUT or PATCH attendances/signout
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async signout({ params: { code }, request, response, auth }) {
+    try {
+      await auth.check();
+
+      const user = await auth.getUser();
+      const student = await user.student().fetch();
+      const { signout_code } = request.all();
+      const query = await Attendance.query()
+        .where("code", code)
+        .andWhere("signout_code", signout_code)
+        .fetch();
+      query.forEach(element => {
+        console.log(element);
+      });
+      return response.status(200).send({
+        payload: {
+          type: "success",
+          message: "sign out"
         }
       });
     } catch (error) {
