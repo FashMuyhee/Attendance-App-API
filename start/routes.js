2c9931cd3a6aae4252daf58b50844dcf943d39c2
fashmuyhee <fashmuyidavid@gmail.com>
2020-03-28 18:19:14 +0100

    added new auth guard for routing

--- start/routes.js
+++ start/routes.js
@@ -20,15 +20,14 @@ Route.get("/", () => {
   return { greeting: "Welcome to Mobile Attendance" };
 });
 
+// student route
 Route.group(() => {
-  // user route
-  Route.get("users/:id", "UserController.show").as("users.show");
-  Route.put("users/:id", "UserController.createAttendance").as(
-    "users.createAttendance"
-  );
-
-  // student route
-  Route.resource("students", "StudentController").apiOnly();
+  Route.resource("students", "StudentController").except([
+    "index",
+    "create",
+    "edit",
+    "store"
+  ]);
   Route.post("students/:id/add_course", "StudentController.addCourse").as(
     "students.addCourse"
   );
@@ -39,8 +38,20 @@ Route.group(() => {
     "students/:id/get_attendance_by_courses",
     "StudentController.getAttendanceByCourses"
   ).as("students.getAttendanceByCourses");
+  Route.put("students/:id/uploadDp", "StudentController.uploadDp").as(
+    "students.uploadDp"
+  );
+  Route.put(
+    "attendances/mark_attendance/:code",
+    "AttendanceController.markAttendance"
+  ).as("attendances.markAttendance");
+  Route.put("attendances/signout", "AttendanceController.signout").as(
+    "attendances.signout"
+  );
+}).middleware(["auth:student"]);
 
-  // lecturer route
+// lecturer route
+Route.group(() => {
   Route.resource("lecturers", "LecturerController").apiOnly();
   Route.post("lecturers/:id/add_course", "LecturerController.addCourse").as(
     "lecturers.addCourse"
@@ -52,8 +63,6 @@ Route.group(() => {
     "lecturers/:id/get_attendances",
     "LecturerController.getAttendance"
   ).as("lecturers.getAttendance");
-
-  // attendance route
   Route.post(
     "attendances/create_attendance",
     "AttendanceController.createAttendance"
@@ -62,14 +71,7 @@ Route.group(() => {
     "attendances/create_signout/:code",
     "AttendanceController.createSignout"
   ).as("attendances.createSignout");
-  Route.put(
-    "attendances/mark_attendance/:code",
-    "AttendanceController.markAttendance"
-  ).as("attendances.markAttendance");
-  Route.put("attendances/signout", "AttendanceController.signout").as(
-    "attendances.signout"
-  );
-}).middleware(["auth"]);
+}).middleware(["auth:lecturer"]);
 
 // course route
 Route.resource("courses", "CourseController").apiOnly();
@@ -80,4 +82,7 @@ Route.group(() => {
   // Route.post("logout", "UserController.logout").as("auth.logout").middleware(['auth']);
 }).prefix("auth");
 
-// attendance
+// authentication
+Route.post("students/login", "StudentController.login").as("students.login");
+Route.post("lecturers/login", "LecturerController.login").as("lecturers.login");
+Route.post("students/", "StudentController.store").as("students.store");
