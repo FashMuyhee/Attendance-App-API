c6102d0d87cbaeebc259e40787c54b1408269ae1
fashmuyhee <fashmuyidavid@gmail.com>
2020-04-17 01:03:23 +0100

    added password hashibg hook

--- app/Models/Lecturer.js
+++ app/Models/Lecturer.js
@@ -2,8 +2,25 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
-
+/** @type {import('@adonisjs/framework/src/Hash')} */
+const Hash = use("Hash");
 class Lecturer extends Model {
+  static boot() {
+    super.boot();
+
+    /**
+     * A hook to hash the user password before saving
+     * it to the database.
+     */
+    this.addHook("beforeCreate", async (userInstance) => {
+      if (userInstance.dirty.password) {
+        userInstance.password = await Hash.make(userInstance.password);
+      }
+    });
+  }
+  static get hidden() {
+    return ["password"];
+  }
   user() {
     return this.belongsTo("App/Models/User");
   }
