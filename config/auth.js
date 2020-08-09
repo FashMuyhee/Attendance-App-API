d7c09ad76a7d5ba28ace8a13837a3242af0ce709
fashmuyhee <fashmuyidavid@gmail.com>
2020-08-09 23:55:00 +0100

    added password to student auth

--- config/auth.js
+++ config/auth.js
@@ -4,102 +4,104 @@
 const Env = use("Env");
 
 module.exports = {
-  /*
-  |--------------------------------------------------------------------------
-  | Authenticator
-  |--------------------------------------------------------------------------
-  |
-  | Authentication is a combination of serializer and scheme with extra
-  | config to define on how to authenticate a user.
-  |
-  | Available Schemes - basic, session, jwt, api
-  | Available Serializers - lucid, database
-  |
-  */
-  authenticator: "student",
+    /*
+      |--------------------------------------------------------------------------
+      | Authenticator
+      |--------------------------------------------------------------------------
+      |
+      | Authentication is a combination of serializer and scheme with extra
+      | config to define on how to authenticate a user.
+      |
+      | Available Schemes - basic, session, jwt, api
+      | Available Serializers - lucid, database
+      |
+      */
+    authenticator: "student",
 
-  /*
-  |--------------------------------------------------------------------------
-  | Session
-  |--------------------------------------------------------------------------
-  |
-  | Session authenticator makes use of sessions to authenticate a user.
-  | Session authentication is always persistent.
-  |
-  */
-  session: {
-    serializer: "lucid",
-    model: "App/Models/User",
-    scheme: "session",
-    uid: "email",
-    password: "password"
-  },
+    /*
+      |--------------------------------------------------------------------------
+      | Session
+      |--------------------------------------------------------------------------
+      |
+      | Session authenticator makes use of sessions to authenticate a user.
+      | Session authentication is always persistent.
+      |
+      */
+    session: {
+        serializer: "lucid",
+        model: "App/Models/User",
+        scheme: "session",
+        uid: "email",
+        password: "password",
+    },
 
-  /*
-  |--------------------------------------------------------------------------
-  | Basic Auth
-  |--------------------------------------------------------------------------
-  |
-  | The basic auth authenticator uses basic auth header to authenticate a
-  | user.
-  |
-  | NOTE:
-  | This scheme is not persistent and users are supposed to pass
-  | login credentials on each request.
-  |
-  */
-  basic: {
-    serializer: "lucid",
-    model: "App/Models/User",
-    scheme: "basic",
-    uid: "email",
-    password: "password"
-  },
+    /*
+      |--------------------------------------------------------------------------
+      | Basic Auth
+      |--------------------------------------------------------------------------
+      |
+      | The basic auth authenticator uses basic auth header to authenticate a
+      | user.
+      |
+      | NOTE:
+      | This scheme is not persistent and users are supposed to pass
+      | login credentials on each request.
+      |
+      */
+    basic: {
+        serializer: "lucid",
+        model: "App/Models/User",
 
-  /*
-  |--------------------------------------------------------------------------
-  | Jwt
-  |--------------------------------------------------------------------------
-  |
-  | The jwt authenticator works by passing a jwt token on each HTTP request
-  | via HTTP `Authorization` header.
-  |
-  */
-  student: {
-    serializer: "lucid",
-    model: "App/Models/Student",
-    scheme: "jwt",
-    uid: "matric_no",
-    options: {
-      secret: Env.get("APP_KEY")
-      // expiresIn: "200000"
-    }
-  },
-  lecturer: {
-    serializer: "lucid",
-    model: "App/Models/Lecturer",
-    scheme: "jwt",
-    uid: "email",
-    password: "password",
-    options: {
-      secret: Env.get("APP_KEY")
-      // expiresIn: "200000"
-    }
-  },
+        scheme: "basic",
+        uid: "email",
+        password: "password",
+    },
 
-  /*
-  |--------------------------------------------------------------------------
-  | Api
-  |--------------------------------------------------------------------------
-  |
-  | The Api scheme makes use of API personal tokens to authenticate a user.
-  |
-  */
-  api: {
-    serializer: "lucid",
-    model: "App/Models/User",
-    scheme: "api",
-    uid: "email",
-    password: "password"
-  }
+    /*
+      |--------------------------------------------------------------------------
+      | Jwt
+      |--------------------------------------------------------------------------
+      |
+      | The jwt authenticator works by passing a jwt token on each HTTP request
+      | via HTTP `Authorization` header.
+      |
+      */
+    student: {
+        serializer: "lucid",
+        model: "App/Models/Student",
+        scheme: "jwt",
+        uid: "matric_no",
+        password: "password",
+        options: {
+            secret: Env.get("APP_KEY"),
+            // expiresIn: "200000"
+        },
+    },
+    lecturer: {
+        serializer: "lucid",
+        model: "App/Models/Lecturer",
+        scheme: "jwt",
+        uid: "email",
+        password: "password",
+        options: {
+            secret: Env.get("APP_KEY"),
+            // expiresIn: "200000"
+        },
+    },
+
+    /*
+      |--------------------------------------------------------------------------
+      | Api
+      |--------------------------------------------------------------------------
+      |
+      | The Api scheme makes use of API personal tokens to authenticate a user.
+      |
+      */
+    api: {
+        serializer: "lucid",
+        model: "App/Models/User",
+        scheme: "api",
+        uid: "email",
+        password: "password",
+    },
 };
