a559524d9cecc526209ecb017a3f8ced396ae214
fashmuyhee <fashmuyidavid@gmail.com>
2020-08-09 23:55:37 +0100

    enabled password hashing

--- app/Models/Student.js
+++ app/Models/Student.js
@@ -2,17 +2,34 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
-
+const Hash = use('Hash')
 class Student extends Model {
-  user() {
-    return this.belongsTo("App/Models/User");
-  }
+    static boot() {
+        super.boot();
 
-  courses() {
-    return this.belongsToMany("App/Models/Course")
-      .pivotTable("student_courses")
-      .withTimestamps();
-  }
+        /**
+         * A hook to hash the user password before saving
+         * it to the database.
+         */
+        this.addHook("beforeCreate", async(userInstance) => {
+            if (userInstance.dirty.password) {
+                userInstance.password = await Hash.make(userInstance.password);
+            }
+        });
+    }
+    static get hidden() {
+        return ["password"];
+    }
+
+    user() {
+        return this.belongsTo("App/Models/User");
+    }
+
+    courses() {
+        return this.belongsToMany("App/Models/Course")
+            .pivotTable("student_courses")
+            .withTimestamps();
+    }
 }
 
 module.exports = Student;
