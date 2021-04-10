27192b894b2f5a03f553a6c4e214bad9b38287f0
FashMuyhee <fashmuyidavid@gmail.com>
2021-04-10 03:09:41 +0100

    added cloudinary service

---
+++ app/Services/Cloudinary.js
@@ -0,0 +1,23 @@
+const cloudinary = require("cloudinary");
+const Env = use("Env");
+
+cloudinary.config({
+  cloud_name: Env.get("CLOUDINARY_CLOUD_NAME"),
+  api_key: Env.get("CLOUDINARY_API_KEY"),
+  api_secret: Env.get("CLOUDINARY_API_SECRET"),
+});
+
+const uploadFile = async (file) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let response = await cloudinary.v2.uploader.upload(file.tmpPath, {
+        folder: "profile_upload",
+      });
+      resolve({ status: true, data: response.secure_url });
+    } catch (error) {
+      console.log(error);
+      reject({ status: false, data: error.message });
+    }
+  });
+};
+module.exports = { uploadFile };
