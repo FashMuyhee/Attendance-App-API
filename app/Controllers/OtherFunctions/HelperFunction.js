bbebbee19e385cc8e565c802f2f08f95f5a555f7
FashMuyhee <fashmuyidavid@gmail.com>
2021-07-14 19:11:08 +0100

    feat: check if student has registered b4

--- app/Controllers/OtherFunctions/HelperFunction.js
+++ app/Controllers/OtherFunctions/HelperFunction.js
@@ -11,4 +11,29 @@ const objIsEmpty = (obj) => {
   return true;
 };
 
-module.exports = { objIsEmpty };
+/**
+ * Check if auth student has registered for
+ * the current marking attendance course
+ * @param  {} {course_id
+ * @param  {} courses}
+ * @returns boolean true /false
+ */
+const isCourseRegistered = ({ course_id, courses = [] }) => {
+  const isExist = courses.find((el) => el.id === course_id);
+  return isExist ? true : false;
+};
+
+/**
+ * A function check if an attendance code has expired
+ ** @param code_created_at
+ */
+const isCodeExpiry = (code_created_at) => {
+  const now = new Date();
+  let diff = (code_created_at.getTime() - now.getTime()) / 1000;
+  diff /= 3600;
+
+  const rndDiff = Math.abs(Math.round(diff));
+  return rndDiff <= 60 ? true : false;
+};
+
+module.exports = { objIsEmpty, isCourseRegistered, isCodeExpiry };
